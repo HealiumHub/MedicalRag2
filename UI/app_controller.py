fcd0fdf1b7eccb06ffa1fc5766b88e5484395aa2
Hồ Lê Minh Thạch <thach.holeminh@gmail.com>
2024-04-24 01:29:26 +0700

    feat: add simple UI

--- UI/app_controller.py
+++ UI/app_controller.py
@@ -1,8 +1,7 @@
+import threading
 import streamlit as st
 
 from UI.const import MODELS
-from completion import QuotedAnswer, get_answer_with_context
-
 
 
 class AppController:
@@ -11,125 +10,96 @@ class AppController:
         self.__render()
 
     def __config(self):
-        if "messages_dict" not in st.session_state:
-            st.session_state.messages_dict = {}
-        if "related_articles" not in st.session_state:
-            st.session_state.related_articles = {}
-        # Init thread and message
-        if "thread_id" not in st.session_state:
-            st.session_state.thread_id = 1
-            st.session_state.thread_ids.append(st.session_state.thread_id)
-            st.session_state.messages = st.session_state.messages_dict.get(
-                st.session_state.thread_id, []
-            )
-        # Initialize messages if not present.
-        if "messages" not in st.session_state:
-            st.session_state.messages = st.session_state.messages_dict.get(
-                st.session_state.thread_id, []
-            )
-
-    def __render(self):
         st.set_page_config(
             page_title="HealthLight Project",
             page_icon="⛑️",
             layout="wide",
-            initial_sidebar_state="expanded",
         )
 
-        # Select model
-        selected_model = st.sidebar.selectbox("Choose completion model", MODELS)
+        if "threads" not in st.session_state:
+            st.session_state.threads = []
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+        if "thread_id" not in st.session_state:
+            st.session_state.thread_id = 0
+            st.session_state.threads.append(st.session_state.thread_id)
 
-        # New threads
-        st.sidebar.divider()
-        if st.sidebar.button(label="New thread", type="primary", use_container_width=True):
-            st.session_state.thread_id = len(st.session_state.thread_ids) + 1
-            st.session_state.thread_ids.append(st.session_state.thread_id)
-            st.session_state.messages = st.session_state.messages_dict.get(
-                st.session_state.thread_id, []
-            )
-        # Update thread messages.
-        st.session_state.messages_dict[
-            st.session_state.thread_id
-        ] = st.session_state.messages
+    def __render(self):
+        self.__render_sidebar()
+        self.__render_main()
 
-        for i, thread_id in enumerate(st.session_state.thread_ids):
-            if thread_id == st.session_state.thread_id:
-                st.sidebar.button(
-                    f"**Thread {i + 1} :point_left:**",
-                    on_click=self.change_thread,
-                    args=(thread_id,),
-                    use_container_width=True,
-                )
-            else:
-                st.sidebar.button(
-                    f"Thread {i + 1}",
-                    on_click=self.change_thread,
-                    args=(thread_id,),
-                    use_container_width=True,
-                )
-        # Title
-        left_col, right_col = st.columns([0.5, 0.5], gap="medium")
-        left_col.markdown("### MedLight")
-        right_col.markdown("### References")
-
-        if len(st.session_state.related_articles[st.session_state.thread_id]) > 0:
-            right_col.markdown(
-                "\n".join(
-                    [
-                        f'##### [{i + 1}. DOI {x["file_name"]}, {x["file_name"]}]()'
-                        + f"\n {self.format_content(x)}\n***"
-                        for i, x in enumerate(
-                        st.session_state.related_articles[st.session_state.thread_id]
-                    )
-                    ]
-                )
-            )
-        else:
-            pass
-
-        # Render the messages
-        for msg in st.session_state.messages:
-            role = msg.role if not isinstance(msg, dict) else msg["role"]
-            content: str = (
-                msg.content[0].text.value
-                if not isinstance(msg, dict)
-                else msg["content"][0]["text"]["value"]
-            )
-
+    def __render_main(self):
+        st.header("How can I help you today? 😄", divider="rainbow")
+        for message in st.session_state.get("messages", []):
+            role = message["role"]
+            content = message["content"]
             if content is None:
                 continue
-
-            if isinstance(content, QuotedAnswer):
-                # Render reference buttons.
-                # TODO: Need better UI for this one.
-                with left_col.chat_message(role):
-                    for i, citation in enumerate(content.citations):
-                        st.button(f"[{i+1}] from {citation.source_id}")
-
-                content = str(content)
-
-            # print(f"Role: {role}, Content: {content}")
-
-            # Show disclaimer for assistant messages.
-            if (
-                    role == "assistant"
-                    and "Please consult professional doctor for accurate medical advices."
-                    not in content
+            if role == "system":
+                pass
+            if role == "assistant":
+                content += "\n\n*Please consult professional doctor for accurate medical advices.*"
+            with st.chat_message(
+                role,
+                avatar=(
+                    "assets/michael-avt.jpeg" if role == "user" else "assets/logo.png"
+                ),
             ):
-                content += (
-                    "\n\n*Please consult professional doctor for accurate medical advices.*"
+                st.markdown(content)
+        if user_query := st.chat_input("Enter your question here"):
+            with st.chat_message("user", avatar=f"assets/michael-avt.jpeg"):
+                st.markdown(user_query)
+                st.session_state.messages.append(
+                    {"role": "user", "content": user_query}
                 )
 
-            if role == "system":  # Don't show system messages.
-                pass
-            else:
-                with left_col.chat_message(role):
-                    st.markdown(content)
-    def change_thread(self, thread_id):
-        st.session_state.thread_id = thread_id
-        st.session_state.messages = st.session_state.messages_dict.get(
-            st.session_state.thread_id, []
-        )
+                # with st.spinner("Searching for the references..."):
+                #     stop_event = threading.Event()
+                # with st.spinner("Generating answer..."):
+                #     with st.chat_message("assistant", avatar=st.image("assets/logo.png")):
 
-    def format_content(self, x):
-        return x["content"][:500] + "..." if len(x["content"]) > 500 else x["content"]
+    def __render_sidebar(self):
+        with st.sidebar:
+            st.title("HealthLight ⛑️")
+            st.markdown(
+                """
+                This is a project that aims to provide a conversational AI system that can help you with your health-related questions.
+                """
+            )
+            st.divider()
+            selected_model = st.selectbox(
+                label="Choose LLM model",
+                options=MODELS,
+                label_visibility="collapsed",
+            )
+            selected_chat = st.selectbox(
+                label="Threads",
+                options=st.session_state.get("threads", []),
+                index=st.session_state.thread_id,
+                format_func=lambda x: f"Chat {x + 1}",
+                label_visibility="collapsed",
+            )
+            self.__change_thread(selected_chat)
+            st.button(
+                label="New Chat",
+                type="primary",
+                on_click=self.__new_chat,
+                use_container_width=True,
+            )
+
+    def __new_chat(self):
+        st.session_state.thread_id = len(st.session_state.get("threads", []))
+        st.session_state.threads = st.session_state.get("threads", [])
+        st.session_state.threads.append(st.session_state.thread_id)
+
+    def __change_thread(self, thread_id: int):
+        st.session_state.thread_id = thread_id
+
+
+class ReturnValueThread(threading.Thread):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.result = None
+
+    def run(self):
+        self.result = self._target(*self._args, **self._kwargs)
---
+++ generation/completion.py
@@ -0,0 +1,178 @@
+from typing import List, Optional
+
+import streamlit as st
+from const import MAX_OUTPUT, MODEL_CONTEXT_LENGTH
+from langchain.schema.output_parser import StrOutputParser
+from langchain.output_parsers import PydanticOutputParser
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.llms.ollama import Ollama
+from langchain_core.messages import ChatMessage, HumanMessage, SystemMessage
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.prompts import PromptTemplate
+
+API_KEY = ""
+
+
+class Citation(BaseModel):
+    file_name: str = Field(
+        ...,
+        description="The file name of a SPECIFIC source which justifies the answer.",
+    )
+    quote_in_source: str = Field(
+        ...,
+        description="The VERBATIM quote from the specified source that justifies the answer.",
+    )
+    quote_in_answer: Optional[str] = Field(
+        ...,
+        description="The VERBATIM quote from your answer that refers to this source.",
+    )
+
+    def __str__(self):
+        return (
+            f"from {self.file_name}: '{self.quote_in_source}', '{self.quote_in_answer}'"
+        )
+
+
+class QuotedAnswer(BaseModel):
+    """Answer the user question based only on the given sources, and cite the sources used."""
+
+    answer: str = Field(
+        ...,
+        description="The answer to the user question, which is based on the given sources.",
+    )
+    citations: List[Citation] = Field(
+        ..., description="Citations from the given sources that justify the answer."
+    )
+
+    def __str__(self):
+        # Format the answer to display to user.
+        answer = self.answer
+
+        for i, citation in enumerate(self.citations):
+            # Insert [1], [2], [3], ... to the answer.
+            insert_index = answer.find(citation.quote_in_answer) + len(
+                citation.quote_in_answer
+            )
+            answer = answer[:insert_index] + f" [{i+1}] " + answer[insert_index:]
+
+            answer += f"\n\n[{i+1}] from [{citation.file_name}.pdf](): \n> {citation.quote_in_source}"
+
+        return answer
+
+
+def transform_to_chat_messages(messages):
+    """Transforms a list of messages to a list of ChatMessage objects."""
+
+    # Assistant: AIMessage
+    # User: HumanMessage
+    # System: SystemMessage
+    res = []
+    for msg in messages:
+        # [{'type': 'text', 'text': {'value': completion}}]
+        content = msg["content"][0]["text"]["value"]
+        if content is None:
+            continue
+
+        if msg["role"] == "assistant":
+            res.append(ChatMessage(content=content, role="assistant"))
+        elif msg["role"] == "user":
+            res.append(HumanMessage(content=content))
+        elif msg["role"] == "system":
+            res.append(SystemMessage(content=content))
+    return res
+
+
+def get_answer_with_context(
+    query: str, model_name: str, related_articles: str | list[dict], stream_handler
+):
+    # Initialize models & context length.
+    if model_name in MODEL_CONTEXT_LENGTH:
+        CONTEXT_LENGTH = MODEL_CONTEXT_LENGTH[model_name]
+    else:
+        print(
+            f"Model {model_name} not found in MODEL_CONTEXT_LENGTH, using default value."
+        )
+        CONTEXT_LENGTH = 16385
+
+    if "gpt" in model_name:
+        model = ChatOpenAI(
+            temperature=0,
+            model=model_name,
+            api_key=API_KEY,
+            verbose=True,
+            streaming=True,
+            callbacks=[stream_handler],
+            max_tokens=MAX_OUTPUT,
+        )
+    else:
+        model = Ollama(
+            temperature=0,
+            model=model_name,
+            verbose=True,
+            callbacks=[stream_handler],
+            num_predict=MAX_OUTPUT,
+        )
+
+    system_prompt = f"Here are some research papers that might be relevant: \n\n{related_articles[:1]}"
+    parser = PydanticOutputParser(pydantic_object=QuotedAnswer)
+    user_prompt = f"""
+    Please answer the following medical question and provide relevant references. Question: {query}
+    \n\n
+    {parser.get_format_instructions()}
+    """
+
+    # personality_prompt = "You are MedLight, an assistant developed to help bridge medical research to the public. The system will give you some relevant research articles that you can use. Use your own knowledge if there is no relevant articles given. \n"
+    personality_prompt = "You are a world class algorithm to answer questions with correct and exact citations\n"
+
+    # This is to avoid exceeding the maximum context length.
+    # Different models have different ways to count token.
+    # This only approximates it by having 1 tok = 1.8 characters.
+    system_prompt = system_prompt[: int((CONTEXT_LENGTH - MAX_OUTPUT) * 1.8)]
+    # print(f"System prompt: {system_prompt}")
+
+    if "gpt" in model_name:
+        if len(st.session_state.messages) == 0:
+            st.session_state.messages = [
+                {
+                    "role": "system",
+                    "content": [
+                        {"type": "text", "text": {"value": personality_prompt}}
+                    ],
+                }
+            ]
+
+        messages = [
+            # *transform_to_chat_messages(st.session_state.messages),
+            SystemMessage(content=personality_prompt),
+            SystemMessage(content=system_prompt),
+            HumanMessage(content=user_prompt),
+        ]
+    else:
+        # gemini and claude only supports alternating between system and user messages -> no 2 consecutive system prompts.
+        messages = [
+            # TODO: not using context from previous messages.
+            # *transform_to_chat_messages(st.session_state.messages),
+            SystemMessage(content=f"{personality_prompt}\n\n{system_prompt}"),
+            HumanMessage(content=user_prompt),
+        ]
+
+    # chain = model | StrOutputParser()
+    chain = model | parser
+    st.session_state.messages.extend(
+        [
+            {
+                "role": "system",
+                "content": [{"type": "text", "text": {"value": system_prompt}}],
+            },
+            {
+                "role": "user",
+                "content": [  # push in the query for display, NOT the actual prompt passed to LLM.
+                    {"type": "text", "text": {"value": query}}
+                ],
+            },
+        ]
+    )
+
+    answer = chain.invoke(messages)
+    # print(answer.citations)
+    return answer
