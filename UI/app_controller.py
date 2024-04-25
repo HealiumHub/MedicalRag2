4938826496487d0d6ec68dd1bd89f0e0d9595d58
Minh Nguyen <50205705+ABCbum@users.noreply.github.com>
2024-04-25 17:41:55 +0700

    fix lint

--- UI/app_controller.py
+++ UI/app_controller.py
@@ -209,7 +209,7 @@ class AppController:
                 on_click=self.__new_chat,
                 use_container_width=True,
             )
-            
+
             st.divider()
             _ = st.text_area(
                 label="Custom instruction",
